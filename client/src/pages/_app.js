be19364468f60fa9428ae30d7391506b8fb54884
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-05-15 23:53:13 +0530

    added vercel analytics

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }

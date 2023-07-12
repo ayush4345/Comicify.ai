0438cc90996cff1ef74983481992e42f3e928e35
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-07-12 13:13:01 +0530

    added product hunt rank

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,6 +3,7 @@ import Lottie from "react-lottie-player";
 import animationData from '@/../../public/assets/astronout.json';
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import Link from "next/link";
 
 // lottie config
 const Hero = () => {
@@ -17,10 +18,10 @@ const Hero = () => {
       id="home"
       className={`flex md:flex-row flex-col mb-14 ${styles.paddingY}`}
     >
-      <div className="flex  w-full items-center justify-center">
-        {/* <div className="bg-opacity-75 bg-gray-600 w-1/3"></div> */}
-        {/* <div cd className="w-1/3 relative"> */}
-        {/* <div className="absolute inset-0 bg-gradient-to-br from-black to-transparent"></div> */}
+      <div className="flex flex-col w-full items-center justify-center">
+        <div className=" text-base text-white font-semibold bg-[#ff6154] p-3 rounded-xl -skew-y-6 mt-5 px-6">
+          <Link target="_blank" href="https://www.producthunt.com/posts/comicify-ai">#19 Product of the Day on Product Hunt</Link>
+        </div>
         <div className="flex flex-col items-center justify-center">
           <Lottie loop animationData={animationData} play className="h-[69vh]" />
           <button className="bg-teal-600 hover:bg-teal-900 text-white py-3 px-6 rounded-full drop-shadow-2xl font-poppins font-semibold text-2xl" onClick={() => router.push("/dashboard")}>
